ebcb1aa77decb4d3b0450421297ee457ae213be5
Alexander Gordeev <bobmouse.g@gmail.com>
2012-01-27 12:27:11 +0400

    Commit

---
+++ src/main/java/go/spocrawler/util/Bootstrap.java
@@ -0,0 +1,27 @@
+package go.spocrawler.util;
+
+import org.tanukisoftware.wrapper.WrapperListener;
+
+/**
+ * User: bobmouse (bobmouse.g@gmail.com)
+ * Date: 27.01.12
+ * Time: 11:28
+ */
+public class Bootstrap implements WrapperListener{
+
+    public static void main(String[] args) {
+
+    }
+
+    public Integer start(String[] strings) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public int stop(int i) {
+        return 0;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void controlEvent(int i) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
